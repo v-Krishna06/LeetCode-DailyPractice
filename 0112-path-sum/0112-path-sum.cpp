f2e6383221f618c8ea56e19860d2d783bc84f58b
Krishna Verma <kv657677@gmail.com>
2026-08-03 23:37:23 +0530

    Time: 0 ms (100%), Space: 21.6 MB (36.61%) - LeetHub

---
+++ 0112-path-sum/0112-path-sum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(root == NULL)
+            return false;
+
+        // Leaf node
+        if(root->left == NULL && root->right == NULL)
+            return root->val == targetSum;
+
+        return hasPathSum(root->left, targetSum - root->val) ||
+               hasPathSum(root->right, targetSum - root->val);
+    }
+};
