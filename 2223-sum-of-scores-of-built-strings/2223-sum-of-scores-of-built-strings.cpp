a2a5decf18d450c13213698853825d32150563bf
Krishna Verma <kv657677@gmail.com>
2026-09-25 12:19:37 +0530

    Time: 14 ms (58.08%), Space: 24.6 MB (36.07%) - LeetHub

---
+++ 2223-sum-of-scores-of-built-strings/2223-sum-of-scores-of-built-strings.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    long long sumScores(string s) {
+        int n = s.size();
+        vector<int> z(n);
+
+        int l = 0, r = 0;
+
+        for (int i = 1; i < n; i++) {
+
+            if (i <= r) {
+                z[i] = min(r - i + 1, z[i - l]);
+            }
+
+            while (i + z[i] < n && 
+                   s[z[i]] == s[i + z[i]]) {
+                z[i]++;
+            }
+
+            if (i + z[i] - 1 > r) {
+                l = i;
+                r = i + z[i] - 1;
+            }
+        }
+
+        long long ans = n;  // z[0] = n
+
+        for (int i = 1; i < n; i++) {
+            ans += z[i];
+        }
+
+        return ans;
+    }
+};
